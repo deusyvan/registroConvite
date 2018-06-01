fa6f62aff76a87eba4be9706bd527b1b9ddc9f5a
deusyvan <deusyvan@gmail.com>
2018-06-01 17:28:42 -0300

    Insere o codigo do usuário seta a sessão e encaminha para o index

--- cadastrar.php
+++ cadastrar.php
@@ -21,17 +21,23 @@ if(!empty($_POST['email'])) {
     $email = addslashes($_POST['email']);
     $senha = md5($_POST['senha']);
     
+    //Para verificar se já existe uma pessoa com esse e-mail
     $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $sql = $pdo->query($sql);
     
     if($sql->rowCount() <= 0) {
         
+        //Gerando o codigo para o cadastro da pessoa
         $codigo = md5(rand(0,99999).rand(0,99999));
-        $sql = "INSERT INTO usuarios (email, senha) VALUES ('$email', '$senha')";
+        
+        //Inserimos o codigo na query
+        $sql = "INSERT INTO usuarios (email, senha, codigo) VALUES ('$email', '$senha', '$codigo')";
         $sql = $pdo->query($sql);
         
+        //Seta a sessão para logado
         unset($_SESSION['logado']);
         
+        //após inserção do codigo encaminha para o index
         header("Location: index.php");
         exit;
     }
